22006d929f13e76987932a4852336feef77f11f0
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2015-06-19 14:39:54 +0300

    improved benchmarks

--- benchmark/go.js
+++ benchmark/go.js
@@ -1,24 +1,32 @@
 "use strict";
 
 var async = require('async');
+var neoAsync = require('neo-async');
 var waterfall = require('run-waterfall')
+var fall = require('fastfall')();
 var waterfallize = require('../');
 
 suite('waterfall alternatives', function() {
-  // set('iterations', 30000);
-  // set('mintime', 10000);
+  // set('iterations', 100000);
+  // set('mintime', 2000);
   // set('concurrency', 3);
 
-  bench('async.waterfall', function(done) {
-    async.waterfall([
+  bench('neo-async.waterfall', function(done) {
+    neoAsync.waterfall([
       function (callback) {
-        callback(null, 'one', 'two')
+        setImmediate(function() {
+          callback(null, 'one', 'two')
+        });
       },
       function (arg1, arg2, callback) {
-        callback(null, 'three')
+        setImmediate(function() {
+          callback(null, 'three')
+        });
       },
       function (arg1, callback) {
-        callback(null, 'done', 'wohoo')
+        setImmediate(function() {
+          callback(null, 'done', 'wohoo')
+        });
       }
     ], function (err, result1, result2) {
       if (err) { throw err; }
@@ -27,16 +35,22 @@ suite('waterfall alternatives', function() {
     })
   });
 
-  bench('run-waterfall', function(done) {
-    waterfall([
+  bench('fastfall', function(done) {
+    fall([
       function (callback) {
-        callback(null, 'one', 'two')
+        setImmediate(function() {
+          callback(null, 'one', 'two')
+        });
       },
       function (arg1, arg2, callback) {
-        callback(null, 'three')
+        setImmediate(function() {
+          callback(null, 'three')
+        });
       },
       function (arg1, callback) {
-        callback(null, 'done', 'wohoo')
+        setImmediate(function() {
+          callback(null, 'done', 'wohoo')
+        });
       }
     ], function (err, result1, result2) {
       if (err) { throw err; }
@@ -49,15 +63,21 @@ suite('waterfall alternatives', function() {
     var next = waterfallize();
 
     next(function(callback) {
-      callback(null, 'one', 'two');
+      setImmediate(function() {
+        callback(null, 'one', 'two');
+      });
     });
 
     next(function(arg1, arg2, callback) {
-      callback(null, 'three');
+      setImmediate(function() {
+        callback(null, 'three');
+      });
     });
 
     next(function(arg1, callback) {
-      callback(null, 'done', 'wohoo');
+      setImmediate(function() {
+        callback(null, 'done', 'wohoo');
+      });
     });
 
     next(function(err, results) {
@@ -65,5 +85,53 @@ suite('waterfall alternatives', function() {
 
       done();
     });
+  });
+
+  bench('run-waterfall', function(done) {
+    waterfall([
+      function (callback) {
+        setImmediate(function() {
+          callback(null, 'one', 'two')
+        });
+      },
+      function (arg1, arg2, callback) {
+        setImmediate(function() {
+          callback(null, 'three')
+        });
+      },
+      function (arg1, callback) {
+        setImmediate(function() {
+          callback(null, 'done', 'wohoo')
+        });
+      }
+    ], function (err, result1, result2) {
+      if (err) { throw err; }
+
+      done();
+    })
+  });
+
+  bench('async.waterfall', function(done) {
+    async.waterfall([
+      function (callback) {
+        setImmediate(function() {
+          callback(null, 'one', 'two')
+        });
+      },
+      function (arg1, arg2, callback) {
+        setImmediate(function() {
+          callback(null, 'three')
+        });
+      },
+      function (arg1, callback) {
+        setImmediate(function() {
+          callback(null, 'done', 'wohoo')
+        });
+      }
+    ], function (err, result1, result2) {
+      if (err) { throw err; }
+
+      done();
+    })
   });
 });
